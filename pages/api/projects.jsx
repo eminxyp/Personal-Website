57850269e7ff050b43f1cc6cfd6c204c5949c0fe
Emin^^ <136367816+eminxyp@users.noreply.github.com>
2023-11-22 22:20:50 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -9,7 +9,7 @@ export default async (req, res) => {
     },
     {
       id: 2,
-      image: "https://hizliresim.com/mak5z3f.jpg",
+      image: "https://cdn.discordapp.com/attachments/1120773551255978106/1176965296427384914/wojakk1-min.jpg?ex=6570c959&is=655e5459&hm=0a221a58e7d33b2bba300300aa817498461e12323f4dd77a93b2c57059ec9297&.",
       name: "Wojakapk",
       description: "Wojakapk instagram üzerinden yönettiğim bir hesaptır, aktif olarak 54k takipçisi bulunmaktadır.",
       link: "https://instagram.com/wojakapk",
