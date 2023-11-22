a4762d4fe4d014c2fc29c585823190badde629e0
Emin^^ <136367816+eminxyp@users.noreply.github.com>
2023-11-22 21:44:25 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,7 +16,7 @@ export default function Home() {
               <div className="flex flex-col lg:justify-start justify-center items-center lg:items-start mt-5 lg:mt-0 w-full">
                 <div className="flex items-center">
                 <p className="flex items-center text-white text-4xl font-semibold">
-                  Umut Bayraktar
+                  Eminxyp
                 </p>
                   <Tippy content={`Online`} animation="shift-away" arrow={false}>
                     <span className={`ml-2 text-online px-2 py-1 font-normal rounded-md text-sm`}>
@@ -26,11 +26,11 @@ export default function Home() {
             
                 </div>
                 <p className="text-white/50 text-md mt-3">
-I am a professional editor and software developer. I have been professionally interested in community management and development for 5 years, I have high level knowledge and experience in areas such as community management and social media management.
+Merhaba! Ben Emin, 15 yaşındayım ve yazılıma meraklı bir gençim. Web geliştirme konusunda kendimi geliştirmeye çalışıyorum ve yeni şeyler öğrenmeyi seviyorum. Hobilerim arasında kod yazmak, web siteleri oluşturmak ve teknolojiyle ilgili içerikleri takip etmek yer alıyor. Amacım, bu alanda kendimi daha da geliştirerek ileride profesyonel bir yazılımcı olmak. 
                 </p>
               </div>
               <div className={`order-first lg:order-last flex-shrink-0 relative w-[160px] h-[160px] rounded-full pulse-avatar-online `}>
-                <img alt="umutbayraktar" src={`https://media.discordapp.net/attachments/737348411568685066/996841172372824184/YT_AVATAR_NET.jpg`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
+                <img alt="umutbayraktar" src={`https://cdn.discordapp.com/attachments/1120773551255978106/1176938380664250520/sm7qozo.jpeg?ex=6570b048&is=655e3b48&hm=f3ecd07d68899b2d9efc01dc2e495a971538b62290bf79d7a8e3448bda3c54ba&`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
                 <div className={`bg-[#040404] rounded-full px-[4px] py-[1px] flex items-center absolute bottom-0 right-4`}>
                   <Tippy content="Online" animation="shift-away" arrow={false}>
                     <i className={`fad fa-circle text-2xl text-online`} />
@@ -40,7 +40,7 @@ I am a professional editor and software developer. I have been professionally in
               
             </div>
             <br></br>
-            <span style={{ zIndex: '-1' }} className="text-white/5 absolute bottom-3 left-7 text-xl sm:text-2xl md:text-4xl lg:text-3xl font-semibold">Community Manager</span>
+            <span style={{ zIndex: '-1' }} className="text-white/5 absolute bottom-3 left-7 text-xl sm:text-2xl md:text-4xl lg:text-3xl font-semibold">Junior Developer</span>
           </div>
       </div>
 
@@ -56,7 +56,7 @@ I am a professional editor and software developer. I have been professionally in
                   .map((_, __) => (
                     <a
                       key={__}
-                      href={`https://github.com/umutxyp/`+_.name}
+                      href={`https://github.com/eminxyp/`+_.name}
                       target="_blank"
                       rel="noreferrer"
                       className="bg-[#191932]/20 p-4 hover:bg-[#191932]/30 shadow-lg hover:shadow-xl transition-all duration-200 rounded-lg w-full"
